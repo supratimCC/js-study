881639f32253e3c0e0e4f8d91fbcb2542f1bcf3b
supratim.pathak@codeclouds.co.in <supratim.pathak@codeclouds.co.in>
2024-02-21 00:23:38 +0530

    ==

---
+++ V-32_while.js
@@ -0,0 +1,15 @@
+var i = 10
+var j = 20
+while (i> 0) {
+    console.log(i)
+    i--
+}
+
+
+while (true) {
+    console.log(j)
+    if(j==3){
+        break;
+    }
+    j--
+}
---
+++ V-33_nestedwhile.js
@@ -0,0 +1,12 @@
+var i = 0
+
+while (i<3) {
+    i++
+    var j = 0
+    while (j < 5) {
+        j++
+        console.log("j",j)
+    }
+    console.log("i======",i)
+
+}
